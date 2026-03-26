5ded18efb39c6afe75d8d28618a127f574b30476
Liz Elliott <e.elliott@epcc.ed.ac.uk>
2026-03-26 13:47:45 +0000

    Modified test

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,7 +5,7 @@ TARGET = "Hello World!"
 
 def test_hello_prints(capsys: pytest.CaptureFixture[str]) -> None:
     captured = capsys.readouterr()
-    assert captured.out == TARGET + "\n"
+    assert captured.out == TARGET
 
 
 def test_hello_returns() -> None:
